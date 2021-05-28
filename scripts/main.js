8c2927c596fd4ac96a59249a811ee40eab3f3382
DenisGagaev <Shaker8@yandex.ru>
2021-05-28 22:10:22 +0300

    Исправить ошибки ревью

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,29 @@
 const openPopup = document.querySelector('.profile__edit');
 const popup = document.querySelector('.popup');
 const closePopup = popup.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form')
+let nameInput = formElement.querySelector("input[name='profileName']");
+let jobInput = formElement.querySelector("input[name='profileText']");
+let profileName = document.querySelector('.profile__name');
+let profileText = document.querySelector('.profile__text');
 
-
-function closeOpen() {
-    openPopup.addEventListener('click', closeOpen);
-    closePopup.addEventListener('click', closeOpen);
-    popup.classList.toggle('popup_opened');
+// Открыть Popup с заполнением imput/закрыть Popup 
+openPopup.addEventListener('click', open);
+closePopup.addEventListener('click', close);
+function open() {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent
+    jobInput.value = profileText.textContent
 }
-closeOpen()
+function close() {
+    popup.classList.remove('popup_opened');
+}
 
-
-const formElement = document.querySelector('.popup__form')
-const nameInput = popup.querySelector('#nickName')
-const jobInput = popup.querySelector('#aboutme') 
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    let inputs = document.querySelectorAll('input');
-    let profileName = document.querySelector('.profile__name');
-    let profileText = document.querySelector('.profile__text');
-    profileName.textContent = inputs[0].value
-    profileText.textContent = inputs[1].value
-    closeOpen()
+// Сохранение дынных профиля из формы Popup
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value
+    profileText.textContent = jobInput.value
+    close()
 }
 formElement.addEventListener('submit', formSubmitHandler);
