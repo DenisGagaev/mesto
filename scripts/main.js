e2426a6e790b626d20398b9bc033fc34f98e994e
DenisGagaev <Shaker8@yandex.ru>
2021-06-08 20:45:22 +0300

    add a popup photo

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,69 @@
+//Массив с фото
+const initialCards = [
+    {
+        text: 'Москва',
+        link: './images/nikita-karimov-lvJZhHOIJJ4-unsplash.jpg'
+    },
+    {
+        text: 'Санкт-Петербург',
+        link: './images/matvey-yelkin-saint-petersburg.jpg'
+    },
+    {
+        text: 'Владивосток',
+        link: './images/pavel-anoshin-vladivostok.jpg'
+    },
+    {
+        text: 'Куршская Коса',
+        link: './images/artem-beliaikin-kurshkaya-kosa.jpg'
+    },
+    {
+        text: 'Сочи',
+        link: './images/dima-fedorov-sochi.jpg'
+    },
+    {
+        text: 'Казань',
+        link: './images/sasha-yudaev-kazan.jpg'
+    }
+];
+
 const openPopup = document.querySelector('.profile__edit');
 const popup = document.querySelector('.popup');
-const closePopup = popup.querySelector('.popup__close');
-const formElement = document.querySelector('.popup__form')
-let nameInput = formElement.querySelector("input[name='profileName']");
-let jobInput = formElement.querySelector("input[name='profileText']");
-let profileName = document.querySelector('.profile__name');
-let profileText = document.querySelector('.profile__text');
+const closePopup = popup.querySelector('#popup__close');
+const formElement = document.querySelector('#popup__form-profile')
+const nameInput = formElement.querySelector("input[name='profileName']");
+const jobInput = formElement.querySelector("input[name='profileText']");
+const profileName = document.querySelector('.profile__name');
+const profileText = document.querySelector('.profile__text');
+const elements = document.querySelector('.elements')
+const cardTemplate = document.querySelector('#cardTemplate').content;
+const addPopup = document.querySelector('#addpopup')
+const addButton = document.querySelector('.profile__add-button');
+const addClosePopup = document.querySelector('#addpopup__close');
 
-// Открыть Popup с заполнением imput/закрыть Popup 
+//Пройтись по массиву
+function addCards() {
+    initialCards.forEach(addCard)
+}
+//
+function addCard({link, text}) {
+    const elementFoto = cardTemplate.cloneNode(true);
+    elementFoto.querySelector('.element__image').src = link;
+    elementFoto.querySelector('.element__text').innerText = text;
+    elementFoto.querySelector('.element__image').alt = text;
+    elements.prepend(elementFoto);
+};
+//Вызвать фотокарточки из массива
+addCards()
+//Открыть\закрыть попап добавления фотокарточек
+function outFotoPopup() {
+    addPopup.classList.toggle('popup_opened');
+}
+//Добавить новую фотокарточку
+function addPhoto() {
+    
+}
+
+// Открыть Popup текста профиля с заполнением imput/закрыть Popup 
 function enterPopup() {
     popup.classList.add('popup_opened');
     nameInput.value = profileName.textContent
@@ -27,5 +83,7 @@ function formSubmitHandler(evt) {
 formElement.addEventListener('submit', formSubmitHandler);
 
 //Слушатели
+addClosePopup.addEventListener('click', outFotoPopup);
+addButton.addEventListener('click', outFotoPopup);
 openPopup.addEventListener('click', enterPopup);
 closePopup.addEventListener('click', exitPopup);
