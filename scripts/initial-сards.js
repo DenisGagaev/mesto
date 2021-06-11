c39aa47179a63969a444593c03451ffd11144ee7
DenisGagaev <Shaker8@yandex.ru>
2021-06-11 23:00:20 +0300

    Заменить JS код

---
+++ scripts/initial-сards.js
@@ -0,0 +1,27 @@
+//Массив с фото
+const initialCards = [
+    {
+        text: 'Казань',
+        link: './images/sasha-yudaev-kazan.jpg'
+    },
+    {
+        text: 'Сочи',
+        link: './images/dima-fedorov-sochi.jpg'
+    },
+    {
+        text: 'Куршская Коса',
+        link: './images/artem-beliaikin-kurshkaya-kosa.jpg'
+    },
+    {
+        text: 'Владивосток',
+        link: './images/pavel-anoshin-vladivostok.jpg'
+    },
+    {
+        text: 'Санкт-Петербург',
+        link: './images/matvey-yelkin-saint-petersburg.jpg'
+    },
+    {
+        text: 'Москва',
+        link: './images/nikita-karimov-lvJZhHOIJJ4-unsplash.jpg'
+    }
+];
